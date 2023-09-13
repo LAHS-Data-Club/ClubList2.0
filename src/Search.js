99c338fc5b14e0a8946298f668a3c63623bb1035
Sabrina <59457991+syenko@users.noreply.github.com>
2023-09-12 21:25:29 -0700

    Add temporary dropdown component

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,15 @@
+import Dropdown from "./Dropdown";
+
 export default function Search({ onChange }) {
     return (
-        <input
-            type="text"
-            placeholder="Search for clubs!"
-            onChange={onChange}
-            className="width-100 bg-white/50 mb-5 p-3 rounded-md drop-shadow-md focus:outline-none focus:drop-shadow-lg focus:bg-white/70 font-body transition duration-100"
-        />
+        <div>
+            <input
+                type="text"
+                placeholder="Search for clubs!"
+                onChange={onChange}
+                className="width-100 bg-white/50 mb-5 p-3 rounded-md drop-shadow-md focus:outline-none focus:drop-shadow-lg focus:bg-white/70 font-body transition duration-100"
+            />
+            <Dropdown></Dropdown>
+        </div>
     );
 }
