bf39094d5b2530a57238eaddb27834a70932f532
Sabrina <59457991+syenko@users.noreply.github.com>
2023-10-01 14:29:39 -0700

    Revert to using caching and local storage

--- src/firebase/firebaseRepository.js
+++ src/firebase/firebaseRepository.js
@@ -1,34 +1,36 @@
-import { db } from "./firebaseConfiguration.js";
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+//import { db } from "./firebaseConfiguration.js";
+//import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 //import { sanitize } from "../utilities.js";
 
-export const fbClubsCollection = collection(db, "clubs-2023");
+//export const fbClubsCollection = collection(db, "clubs-2023");
 
 export async function readClubsFromFirestore(resolve) {
     const clubs = [];
 
+/*
     const querySnapshot = await getDocs(fbClubsCollection);
 
     querySnapshot.forEach((doc) => {
         let clubData = { ...doc.data(), id: doc.id };
-        // console.log(clubData);
+         console.log(clubData);
 
         clubs.push(clubData);
     });
-    // let request = await fetch("https://cachedclublist.lahsdataclub.com/clubs.json");
-    // let clubsJSON = await request.json();
+*/
+    let request = await fetch("https://cachedclublist.lahsdataclub.com/clubs.json");
+    let clubsJSON = await request.json();
 
-    // for (let id = 0; id < clubsJSON.documents.length; id++) {
-    //     let club = { ...clubsJSON.documents[id].fields };
+    for (let id = 0; id < clubsJSON.documents.length; id++) {
+        let club = { ...clubsJSON.documents[id].fields };
 
-    //     for (let key in club) {
-    //         club[key] = club[key].stringValue;
-    //     }
+        for (let key in club) {
+            club[key] = club[key].stringValue;
+        }
+        
+        club = { ...club, id: id };
 
-    //     club = { ...club, id: id };
-
-    //     clubs.push(club);
-    // }
+        clubs.push(club);
+    }
 
     localStorage.setItem("last_loaded", new Date());
     localStorage.setItem("clubs", JSON.stringify(clubs));
