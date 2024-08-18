9e9b300ef77ff8c1fd59bce88e0f440e3e6b995e
NeilMarwah <111013796+NeilMarwah@users.noreply.github.com>
2024-08-18 13:24:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function App() {
     return (
-        <div className="w-full h-full min-h-[100vh] bg-gradient-to-br from-sky-200 to-blue-300 m-0 p-0">
+        <div className="w-full h-full min-h-[100vh] bg-gradient-to-br from-white to-white m-0 p-0">
             <Outlet></Outlet>
         </div>
     );
